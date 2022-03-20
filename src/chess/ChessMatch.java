7bbf43be635cb1bb9a5ad830617d7f3f91699ab4
Herdeson Mourão <herdesondev@gmail.com>
2022-03-20 18:22:11 -0300

    Feat :sparkles: implementando pawn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -4,6 +4,7 @@ package chess;
 import boardgame.Board;
 import chess.pieces.Rook;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import boardgame.Position;
 import boardgame.Piece;
 
@@ -228,15 +229,23 @@ public class ChessMatch {
 		placeNewPiece('f', 1, new King(board, Color.WHITE));
 		placeNewPiece('g', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
-		placeNewPiece('a', 2, new Rook(board, Color.WHITE));
-		placeNewPiece('b', 2, new Rook(board, Color.WHITE));
-		placeNewPiece('c', 2, new King(board, Color.WHITE));
-		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
-		placeNewPiece('f', 2, new King(board, Color.WHITE));
-		placeNewPiece('g', 2, new Rook(board, Color.WHITE));
-		placeNewPiece('h', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
+		placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
+		placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
+		placeNewPiece('d', 2, new Pawn(board, Color.WHITE));
+		placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
+		placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
+		placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
+		placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 		
+		placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
+		placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
+		placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
+		placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
+		placeNewPiece('e', 7, new Pawn(board, Color.BLACK));
+		placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
+		placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
+		placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
 		placeNewPiece('a', 8, new King(board, Color.BLACK));
 		placeNewPiece('b', 8, new King(board, Color.BLACK));
 		placeNewPiece('c', 8, new King(board, Color.BLACK));
@@ -245,13 +254,5 @@ public class ChessMatch {
 		placeNewPiece('f', 8, new King(board, Color.BLACK));
 		placeNewPiece('g', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
-		placeNewPiece('a', 7, new Rook(board, Color.BLACK));
-		placeNewPiece('b', 7, new Rook(board, Color.BLACK));
-		placeNewPiece('c', 7, new King(board, Color.BLACK));
-		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
-		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
-		placeNewPiece('f', 7, new King(board, Color.BLACK));
-		placeNewPiece('g', 7, new Rook(board, Color.BLACK));
-		placeNewPiece('h', 7, new Rook(board, Color.BLACK));
 	}
 }
